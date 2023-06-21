37d71b0deafe19d837f0e1933f8f075025cd9c08
Roman Zhuravskyi <zhurarom.dev@gmail.com>
2023-06-21 21:11:27 +0200

    feat: parallel running

--- src/run_with_cuda.py
+++ src/run_with_cuda.py
@@ -66,3 +66,64 @@ output = d_output.copy_to_host()
 cv2.imshow("Merged Components", output)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+# OR
+# import cv2
+# import numpy as np
+# import multiprocessing
+
+# def process_component(threshold, label_ids, index):
+#     _, label_map, _, _ = cv2.connectedComponentsWithStats(
+#         threshold, connectivity=4, ltype=cv2.CV_32S)
+#     label_ids[index] = label_map.tobytes()
+
+# # Loading the image
+# img = cv2.imread('data/img.jpg')
+
+# # Preprocess the image by converting it to grayscale
+# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# # Applying 7x7 Gaussian Blur
+# blurred = cv2.GaussianBlur(gray_img, (7, 7), 0)
+
+# # Applying threshold
+# _, threshold = cv2.threshold(
+#     blurred, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+
+# # Split the threshold image into multiple segments
+# num_segments = multiprocessing.cpu_count()
+# segment_height = threshold.shape[0] // num_segments
+# segments = [threshold[i * segment_height : (i + 1) * segment_height, :]
+#             for i in range(num_segments)]
+
+# # Initialize an array to store the label maps
+# label_ids = multiprocessing.Array('b', num_segments * threshold.size)
+
+# # Create a pool of processes
+# pool = multiprocessing.Pool()
+
+# # Process each segment in parallel
+# for i in range(num_segments):
+#     pool.apply_async(process_component, args=(segments[i], label_ids, i))
+
+# # Wait for all processes to finish
+# pool.close()
+# pool.join()
+
+# # Concatenate the label maps from all segments
+# label_map = np.concatenate([np.frombuffer(label_ids, dtype=np.int32)
+#                             for label_ids in label_ids])
+
+# # Merge the components
+# merged_components = np.zeros(img.shape, dtype="uint8")
+# for i in range(1, np.max(label_map) + 1):
+#     componentMask = (label_map == i).astype("uint8") * 255
+#     merged_components = cv2.bitwise_or(merged_components, cv2.merge([componentMask, componentMask, componentMask]))
+
+# # Display the merged components on the input image
+# output = cv2.addWeighted(merged_components, 0.7, img, 0.3, 0)
+
+# cv2.imshow("Merged Components", output)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
